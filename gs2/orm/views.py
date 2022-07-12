ea6b8e4a3573f528812a1372219392d36df98174
Jay Kapadiya <jayk.mobio@gmail.com>
2022-07-12 14:55:37 +0530

    Database display

--- gs2/orm/views.py
+++ gs2/orm/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from orm.models import Student
 
 # Create your views here.
+def Studentinfo(request):
+    stud = Student.objects.all()
+    return render(request,'studentdetails.html',{'stu': stud})
