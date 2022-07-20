906372a3ea2ca34e7ed4d0e0665bba6b11e87469
Jay Kapadiya <jayk.mobio@gmail.com>
2022-07-20 11:32:24 +0530

    m

--- project1/crm1/cbv.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from django.views.generic.base import View
-
-class home(View):
-    def get(self,request):
-        return (request,'dashboard.html')
--- project1/crm1/urls.py
+++ project1/crm1/urls.py
@@ -7,29 +7,25 @@ from .cbv import *
 app_name = 'crm1'
 
 #function base views
-# urlpatterns = [
-#     path('', views.home,name='home'),
-#     path('register/', views.registerPage,name='register'),
-#     path('login/', views.loginPage,name = "login"),
-#     path('logout/', views.logoutuser,name = "logout"),
-#     path('account/', views.accountSettings, name="account"),
-#     path('user/', views.userPage,name='user'),
-#     path('products/', views.products,name='products'),
-#     path('customer/<str:pk>/', views.customer, name="customer"),
-#     path('create_order/<str:pk>',views.createOrder,name='create_order'),
-#     path('udate_order/<str:pk>',views.updateOrder,name='update_order'),
-#     path('delete_order/<str:pk>',views.deleteOrder,name='delete_order'),
-
-#     path('reset_password/',views.PasswordResetView.as_view(),name="reset_password"),
-
-#     path('password_reset/sent/',views.PasswordChangeDoneView.as_view(),name='reset_password_done'),
-
-#     path('reset/<uidb64>/<token>/',views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
-
-#     path('reset_password_complete/',views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
-# ]
-
-#class based views
 urlpatterns = [
-    path('',home.as_view(),name="home"),
+    path('', views.home,name='home'),
+    path('register/', views.registerPage,name='register'),
+    path('login/', views.loginPage,name = "login"),
+    path('logout/', views.logoutuser,name = "logout"),
+    path('account/', views.accountSettings, name="account"),
+    path('user/', views.userPage,name='user'),
+    path('products/', views.products,name='products'),
+    path('customer/<str:pk>/', views.customer, name="customer"),
+    path('create_order/<str:pk>',views.createOrder,name='create_order'),
+    path('udate_order/<str:pk>',views.updateOrder,name='update_order'),
+    path('delete_order/<str:pk>',views.deleteOrder,name='delete_order'),
+
+    path('reset_password/',views.PasswordResetView.as_view(),name="reset_password"),
+
+    path('password_reset/sent/',views.PasswordChangeDoneView.as_view(),name='reset_password_done'),
+
+    path('reset/<uidb64>/<token>/',views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+
+    path('reset_password_complete/',views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
 ]
+
